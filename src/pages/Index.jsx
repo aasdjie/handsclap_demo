569fa41da9c33e7c59b0f93de50acb8ad511945b
韩杰 <aasdjie@163.com>
2020-09-26 21:19:30 +0800

    fixed:修改标题

---
+++ src/pages/Index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>欢迎进入空间</h1>
+    <p>
+      Want to add more pages? Please refer to
+      <a
+        href="https://pro.ant.design/docs/block-cn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        use block
+      </a>
+      。
+    </p>
+  </div>
+);
